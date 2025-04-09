8e173fbe64223beecf4ab901530dc85e5fc766cd
alex25calero <64010795+alex25calero@users.noreply.github.com>
2025-04-09 10:37:14 +0200

    Add files via upload

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,38 @@
 /*   By: alegarci <alegarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 18:36:59 by alegarci          #+#    #+#             */
-/*   Updated: 2025/04/08 18:37:08 by alegarci         ###   ########.fr       */
+/*   Updated: 2025/04/08 23:25:44 by alegarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+			return (char *)&str[i];
+		i++;
+	}
+	if ((char)c == '\0')
+		return (char *)&str[i];
+	return ('\0');
+}
+
+#include <stdio.h>
+int main() {
+    const char *texto;
+    char *resultado;
+
+	texto = "hola mundo";
+	resultado = ft_strchr(texto, 'a');
+    if (resultado != NULL) {
+        printf("Carácter encontrado: '%c' en la posición: %ld\n", *resultado, resultado - texto);
+    } else {
+        printf("Carácter no encontrado.\n");
+    }
+
+    return (0);
+}
