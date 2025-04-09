65d6bb38ba1dcc8cc15a18136c32ddd3d19290d0
alex25calero <alex25calero@gmail.com>
2025-04-09 15:27:15 +0200

    carpeta

---
+++ libft/ft_bzero.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegarci <alegarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 13:39:29 by alegarci          #+#    #+#             */
+/*   Updated: 2025/04/09 13:43:26 by alegarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_bzero(void *s, unsigned int n)
+{
+	unsigned int	i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	return (s);
+}
+
+#include <stdio.h> 
+int main(void)
+{
+    char str[10] = "Hello!";
+
+    printf("Antes de ft_bzero: %s\n", str);
+
+    // Llamamos a ft_bzero para poner a cero los primeros 5 bytes
+    ft_bzero(str, 5);
+
+    printf("Después de ft_bzero: %s\n", str);  // Debería mostrar un string vacío
+
+    return 0;
+}
