a57812d8c7d82f8e0f3d6ee368e7b096643b0e17
alex25calero <alex25calero@gmail.com>
2025-04-14 16:04:14 +0200

    actualizado

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegarci <alegarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 15:08:20 by alegarci          #+#    #+#             */
+/*   Updated: 2025/04/14 16:04:02 by alegarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*res;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (NULL);
+	res = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+/*
+#include <stdio.h>
+
+char	toUpper(unsigned int i,char c)
+{
+	if (c >= 'a' && c <= 'z')
+		c = c - 32;
+	return (c);
+}
+
+int main(void)
+{
+	char *st1 = "hola mundo";
+	char *res = ft_strmapi(st1, toUpper);
+
+	printf ("%s\n", st1);
+	printf ("%s", res);
+}
+*/
